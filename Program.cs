a3511a99f2d985b0950b4e899e54779997115060
Rifqi <rifqimohammad790@gmail.com>
2024-12-29 20:20:53 +0700

    Konfigurasi Dasar Views dan Controllers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+// Menambahkan Middleware MVC
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+// app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/", () => "Hello World!");
+//Konfigurasi Request Http
+app.UseStaticFiles();
 
+//Konfigurasi Routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{Action=Index}/{Id?}");
 app.Run();
--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a1ca8164866d2d4a614f62e40c8d1969577393")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
