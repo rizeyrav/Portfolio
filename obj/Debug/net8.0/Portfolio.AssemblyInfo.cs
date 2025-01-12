32bd406bf853abd1ebf8ffb9dcbb8c9a74e813f3
Rifqi <rifqimohammad790@gmail.com>
2025-01-12 20:32:37 +0700

    Konfigurasi Style Dasar dan Navbar

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a1ca8164866d2d4a614f62e40c8d1969577393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3511a99f2d985b0950b4e899e54779997115060")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
