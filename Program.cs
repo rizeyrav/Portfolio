43d79af654449cea1cd65785ac4f39e7625479fa
Rifqi <rifqimohammad790@gmail.com>
2025-01-27 15:04:58 +0700

    Migrasi Konfigurasi database di program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Menambahkan Middleware MVC
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
-// app.MapGet("/", () => "Hello World!");
+
+// Mengambil Konfigurasi DefaultConnection
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+// Menambahkan file dbContext ke builder
+builder.Services.AddDbContext<PortfoliodbContext>
+// Menambahkan koneksi kedatabase
+(options => options.UseNpgsql(connection));
+
 
 //Konfigurasi Request Http
 app.UseStaticFiles();
+
 //Menambahkan Routing
 app.UseRouting();
 
